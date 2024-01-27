e166c75934f97cb1e3eea12e11b534c7ed8266b9
Sanyam Gupta <sanyam23.sg@gmail.com>
2024-01-27 15:47:36 -0800

    added code

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          changed
+          New Code development
         </a>
       </header>
     </div>
